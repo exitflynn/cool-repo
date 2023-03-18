4b21a3ba6a49199336dad5893fa591b919acfc96
Akshit Tyagi <tyagiakshit833@gmail.com>
2023-03-18 09:54:54 +0530

    cooler commit

--- binS.c
+++ binS.c
@@ -10,14 +10,14 @@ int binarySearch(int arr[], int l, int r, int x)
 			return binarySearch(arr, l, mid - 1, x);
 		return binarySearch(arr, mid + 1, r, x);
 	}
-	return -1;
+	return -1
 }
 
 int main()
 {
 	int arr[] = { 2, 3, 4, 10, 40 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = 10
+	int x = 10;
 	int result = binarySearch(arr, 0, n - 1, x);
 	(result == -1)
 		? printf("Element is not present in array")
